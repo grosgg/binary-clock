f94dc10d31513ba382cfd487a915ea8c12c5b90c
Jeremy Marin <marin.jeremy@gmail.com>
2020-07-05 22:48:52 +0900

    Version 0.1

---
+++ src/main.cpp
@@ -0,0 +1,91 @@
+#include <Arduino.h>
+#include <NTPClient.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <WEMOS_Matrix_GFX.h>
+#include <TimeLib.h>
+#include "wifi_settings.h"
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "jp.pool.ntp.org", 3600 * 9, 3600000);
+MLED matrix(0);
+
+const unsigned char loading [] = { 0x00, 0x54, 0x50, 0x74, 0x54, 0x54, 0x00, 0x00 };
+const int hourY = 0;
+const int hourBits = 5;
+const int minuteY = 2;
+const int minuteBits = 6;
+const int secondY = 4;
+const int secondBits = 6;
+const int dayY = 6;
+const int dayBits = 5;
+const int monthY = 7;
+const int monthBits = 4;
+
+void setupWifi() {
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  ESP.eraseConfig();
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void setupClock() {
+  timeClient.begin();
+  timeClient.update();
+  Serial.println(timeClient.getFormattedTime());
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Hi!");
+
+  // Display loading screen while setting up wifi and clock
+  matrix.clear();
+  matrix.drawBitmap(0, 0, loading, 8, 8, LED_ON);
+  matrix.writeDisplay();
+
+  setupWifi();
+  setupClock();
+}
+
+void drawTimeUnit(int value, int row, int length) {
+  int binary [6] = {};
+  for (int i = 0; value > 0; i++) {
+    binary[i] = value % 2;
+    value = value / 2;
+  }
+
+  for (int i = 0; i < length; i++) {
+    if (binary[i]) {
+      matrix.drawPixel(i, row, LED_ON);
+    }
+  }
+}
+
+void loop()
+{
+  matrix.clear();
+  setTime(timeClient.getEpochTime());
+  time_t t = now();
+
+  drawTimeUnit(hour(t), hourY, hourBits);
+  drawTimeUnit(minute(t), minuteY, minuteBits);
+  drawTimeUnit(second(t), secondY, secondBits);
+  drawTimeUnit(day(t), dayY, dayBits);
+  drawTimeUnit(month(t), monthY, monthBits);
+  matrix.drawPixel(7, weekday(t) - 1, LED_ON);
+
+  matrix.writeDisplay();
+  delay(500);
+}
